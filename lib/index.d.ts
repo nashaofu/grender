bc73a11f42611995a4933f7b15739bbb8f35bbdd
nashaofu <diaocheng@outlook.com>
2020-03-08 18:18:15 +0800

    delete 删除lib文件夹下的文件

--- lib/index.d.ts
+++
@@ -1,6 +0,0 @@
-import Shape from './shape';
-import GRender from './grender';
-import Rect from './shapes/rect';
-import Circle from './shapes/circle';
-import Ellipse from './shapes/ellipse';
-export { Shape, Rect, Circle, Ellipse, GRender as default };
--- lib/matrix.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * 二维度坐标矩阵乘法
- * @param {number[6]} m1
- * @param {number[6]} m2
- */
-export declare function multiply(m1: number[], m2: number[]): number[];

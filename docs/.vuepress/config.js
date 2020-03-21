3e5b0380ddf4d3aed6dd812b2ce33030faf19b07
nashaofu <diaocheng@outlook.com>
2020-03-21 23:34:14 +0800

    chore 编写文档

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  title: 'GRender',
+  description: '🛠️ A lightweight canvas library for 2D.',
+  base: '/grender/',
+  themeConfig: {
+    logo: '/logo.png',
+    sidebar: 'auto',
+    smoothScroll: true,
+    repo: 'nashaofu/grender',
+    editLinks: true,
+    docsDir: 'docs',
+    editLinkText: '在 GitHub 上编辑此页',
+    nav: [
+      {
+        text: '指南',
+        link: '/guide/'
+      },
+      {
+        text: 'API',
+        link: '/api/'
+      }
+    ]
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,35 @@
+---
+home: true
+heroImage: /logo.png
+actionText: 快速上手 →
+actionLink: /guide/
+---
+
+## 起步
+
+安装：
+
+```bash
+npm i grender
+# or
+yarn add grender
+```
+
+上手
+
+```js
+import GRender, { Rect } from 'grender'
+
+const grender = new GRender(document.querySelector('#app'))
+const rect = new Rect({
+  shape: {
+    x: 0,
+    y: 0,
+    width: 40,
+    height: 80
+  },
+  z: 2
+})
+
+grender.add(rect)
+```
